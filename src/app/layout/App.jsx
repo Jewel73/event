e56c52110edfa8f8444f8b23d3c9dc3659cafedc
jewel73 <jewelranar420@gmail.com>
2020-10-31 20:13:26 +0600

    edited project

--- src/app/layout/App.jsx
+++ src/app/layout/App.jsx
@@ -1,11 +1,17 @@
 import './style.css';
-
+import React from 'react'
+import {Button} from 'semantic-ui-react'
 function App() {
-  return (
-    <div className="App">
-      <h1>Events</h1>
-    </div>
-  );
+    return (
+        <div className="App">
+            <h1>Events</h1>
+
+            <button className= 'ui con red button'>
+                <i className='user icon'/> User
+            </button>
+            <Button toggle icon='user' content='React Button' color='compact'  />
+        </div>
+    )
 }
 
 export default App;
---
+++ src/features/events/EventDashboard/EventDashboard.jsx
@@ -0,0 +1,8 @@
+
+// export default function EventDashboard(){
+//     return (
+//         <Grid>
+
+//         </Grid>
+//     )
+// }
